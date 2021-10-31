7c5710400972ee6c6b212c93a9e8c6be71ebaa81
Ajij_Khaan <ajijkhaan28@gmail.com>
2021-10-31 14:17:28 +0600

    second

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express')
+var cors = require('cors')
+require('dotenv').config()
+const { MongoClient } = require('mongodb');
+
+
 const app = express()
-const port = process.emitWarning.PORT || 5000;
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ludk7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('db success cinect')
+    }
+    finally {
+        await client.close();
+    }
+
+}
+
+run().catch(console.dirr)
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Tour BUddy Server iS running');
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Server Running at Port:${port}`)
 })
